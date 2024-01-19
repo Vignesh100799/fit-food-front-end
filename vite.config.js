a4cbafbf671174c07b8f2e36ab19c391d549eb95
Vignesh100799 <baluvignesh422@gmail.com>
2024-01-19 17:38:09 +0530

    api added

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server : {
     proxy : {
       '/api' : {
-        target : 'http://localhost:6505'
+        target : 'https://fit-food.onrender.com'
       }
     }
   }
